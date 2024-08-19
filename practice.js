ac529347e0fdb404a8ad66e2e4b544b2f90ac0f5
OladunjoyeSamuel <oladunjoyesamuel153@gmail.com>
2024-08-19 12:29:23 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,78 @@
+// function sam(a, b) {
+//     return [a+b + "<br>" + a*b]
+// }
+
+// let me = sam(3, 7)
+// let food = document.getElementById('para1')
+
+// food.innerHTML = me
+
+
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32);
+//   }
+  
+//   let value = toCelsius(77);
+//   let food = document.getElementById('para1')
+//   food.innerHTML = value
+
+//   document.getElementById('para1').innerHTML=(value)
+
+
+// const sam=(a, b) =>{
+//     return b**a
+// }
+
+// let me = sam(3, 7)
+// let food = document.getElementById('para1')
+
+// food.innerHTML = me
+
+//document.getElementById('para1').innerHTML=(me)
+
+// function maths1(a, b) {
+//     return [a*b]
+// }
+// let work1 = maths1(2, 5)
+
+// const human = {
+// height : 60,
+// age :30,
+// head: {
+//     eyecolor:"blue",
+//     nose: "wide",
+//     mouth: "small"
+// },
+// }
+
+// human.gender = "female"
+// human.name = "shola"
+// human.age = "24"
+
+// console.log(human.name + " has a very " + 
+//     human.head.nose + " nose" + " and she is " + 
+//     human.age + " years old")
+
+    const worker = {
+        name : {
+        firstname: 'Samuel',
+        lastname: ' Oladunjoye'
+    },
+        age : 17,
+        work : {
+        workplace: 'SafeHands Coding Center',
+        location: {
+            street: ', Awolowo Road',
+            area: ', Bodija',
+        },
+    },
+        hobbies : {
+        workhub : 'Coding', 
+        recreation: ', Reading',
+        sports: ', Badminton'
+    },
+    };
+
+    function myfunction() {
+        document.getElementById('para3').innerHTML = 'Hello World';
+    }
